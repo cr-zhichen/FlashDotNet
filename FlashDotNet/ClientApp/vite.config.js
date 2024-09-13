c6f224f1b8fa0f6857dab02ba573c6be80c23766
zgccrui <zgccrui@outlook.com>
2024-09-13 14:41:58 +0800

    修复vue项目启动端口错误的问题

--- FlashDotNet/ClientApp/vite.config.js
+++ FlashDotNet/ClientApp/vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
     },
     server: {
         host: '0.0.0.0',
+        port: process.env.PORT || 5173, // 优先使用命令行或环境变量指定的端口
     },
     build: {
         outDir: '../wwwroot',
