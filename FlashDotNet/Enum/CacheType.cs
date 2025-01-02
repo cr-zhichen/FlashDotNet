9d7793c26c82b45202b3cc85b87d074e78bab973
zgccrui <zgccrui@outlook.com>
2025-01-02 11:39:41 +0800

    ✨ feat(缓存): 添加内存和Redis缓存支持，并配置EFCore二级缓存拦截器

---
+++ FlashDotNet/Enum/CacheType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlashDotNet.Enum;
+
+/// <summary>
+/// 缓存类型 
+/// </summary>
+public enum CacheType
+{
+    /// <summary>
+    /// 内存缓存
+    /// </summary>
+    [Display(Name = "memory", Description = "内存缓存")]
+    Memory,
+
+    /// <summary>
+    /// Redis缓存
+    /// </summary>
+    [Display(Name = "redis", Description = "Redis缓存")]
+    Redis
+}
--- FlashDotNet/Program.cs
+++ FlashDotNet/Program.cs
@@ -1,6 +1,8 @@
 #region Using
 
 using System.Reflection;
+using EasyCaching.Core.Configurations;
+using EFCoreSecondLevelCacheInterceptor;
 using FlashDotNet.Data;
 using FlashDotNet.Enum;
 using FlashDotNet.Filter;
@@ -183,38 +185,113 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 
 #region 数据库连接配置
 
-var databaseOptions = (builder.Configuration.GetSection("DefaultConnection").Get<string>() ?? "").ToLower();
+var databaseOptions = (builder.Configuration.GetSection("DatabaseConnection").Get<string>() ?? "").ToLower();
 
-if (databaseOptions == nameof(DatabaseType.Mysql).ToLower())
+// 在程序启动时打印使用的数据库类型
+if (databaseOptions == DatabaseType.Mysql.GetDisplayName().ToLower())
 {
     Console.WriteLine("使用MySQL数据库");
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
-            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySqlConnection"))));
 }
-else if (databaseOptions == nameof(DatabaseType.Postgresql).ToLower())
+else if (databaseOptions == DatabaseType.Postgresql.GetDisplayName().ToLower())
 {
     Console.WriteLine("使用PostgreSQL数据库");
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 }
-else if (databaseOptions == nameof(DatabaseType.Sqlite).ToLower())
+else if (databaseOptions == DatabaseType.Sqlite.GetDisplayName().ToLower())
 {
     Console.WriteLine("使用Sqlite数据库");
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 }
-else if (databaseOptions == nameof(DatabaseType.Sqlserver).ToLower())
+else if (databaseOptions == DatabaseType.Sqlserver.GetDisplayName().ToLower())
 {
     Console.WriteLine("使用SqlServer数据库");
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
 }
 else
 {
-    Console.WriteLine("使用Sqlite数据库");
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlite($"Data Source={Path.Combine(baseDirectory, "App.db")}"));
+    Console.WriteLine("使用默认值：Sqlite数据库");
+}
+
+builder.Services.AddDbContext<AppDbContext>((serviceProvider, optionsBuilder) =>
+{
+    // 根据配置选择数据库类型
+    if (databaseOptions == DatabaseType.Mysql.GetDisplayName().ToLower())
+    {
+        optionsBuilder.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySqlConnection")));
+    }
+    else if (databaseOptions == DatabaseType.Postgresql.GetDisplayName().ToLower())
+    {
+        optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection"));
+    }
+    else if (databaseOptions == DatabaseType.Sqlite.GetDisplayName().ToLower())
+    {
+        optionsBuilder.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+    }
+    else if (databaseOptions == DatabaseType.Sqlserver.GetDisplayName().ToLower())
+    {
+        optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection"));
+    }
+    else
+    {
+        optionsBuilder.UseSqlite($"Data Source={Path.Combine(baseDirectory, "App.db")}");
+    }
+
+    // 添加 EFCore Second Level Cache 拦截器
+    var cacheInterceptor = serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>();
+    optionsBuilder.AddInterceptors(cacheInterceptor);
+
+    // 可选：配置查询跟踪行为
+    optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+});
+
+#endregion
+
+#region 配置 EFCore Second Level Cache 服务
+
+builder.Services.AddEFSecondLevelCache(options =>
+{
+    // 使用内存缓存提供程序
+    options.UseEasyCachingCoreProvider("cache", isHybridCache: false)
+        .ConfigureLogging(true) // 启用日志记录（根据需要设置）
+        .UseDbCallsIfCachingProviderIsDown(TimeSpan.FromMinutes(1))
+        .CacheAllQueries(CacheExpirationMode.Absolute, TimeSpan.FromHours(1)) // 缓存所有查询，过期时间为1小时
+        .UseCacheKeyPrefix("EF_"); // 可选：设置缓存键前缀
+});
+
+#endregion
+
+#region 缓存数据库配置
+
+var cacheDatabaseOptions = (builder.Configuration.GetSection("CacheConnection").Get<string>() ?? "").ToLower();
+
+if (cacheDatabaseOptions == CacheType.Redis.GetDisplayName().ToLower())
+{
+    Console.WriteLine("使用Redis缓存");
+    builder.Services.AddEasyCaching(options =>
+    {
+        options.UseRedis(config =>
+        {
+            config.DBConfig.Endpoints.Add(new ServerEndPoint(
+                builder.Configuration.GetSection("RedisConnection:Host").Get<string>() ?? "localhost",
+                builder.Configuration.GetSection("RedisConnection:Port").Get<int>()));
+            config.DBConfig.Password = builder.Configuration.GetSection("RedisConnection:Password").Get<string>();
+            config.DBConfig.AsyncTimeout = builder.Configuration.GetSection("RedisConnection:ConnectionTimeout").Get<int>();
+            config.DBConfig.AllowAdmin = builder.Configuration.GetSection("RedisConnection:AllowAdmin").Get<bool>();
+            config.DBConfig.IsSsl = builder.Configuration.GetSection("RedisConnection:Ssl").Get<bool>();
+            config.DBConfig.SslHost = builder.Configuration.GetSection("RedisConnection:SslHost").Get<string>();
+            config.DBConfig.Database = builder.Configuration.GetSection("RedisConnection:Database").Get<int>();
+
+            config.SerializerName = "json";
+
+        }, "cache").WithJson();
+    });
+}
+else
+{
+    Console.WriteLine("使用默认值：内存缓存");
+    builder.Services.AddEasyCaching(options =>
+    {
+        options.UseInMemory(config => { config.SerializerName = "json"; }, "cache")
+            .WithJson();
+    });
 }
 
 #endregion
@@ -346,13 +423,14 @@ app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 using var serviceScope = app.Services.CreateScope();
 var context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+// context.Database.Migrate();
 context.Database.EnsureCreated();
 
 #endregion
 
 #region 静态文件配置
 
-var resourcesPath = Path.Combine(Path.Combine(baseDirectory, "Resources"));
+var resourcesPath = Path.Combine(Path.Combine(baseDirectory, "uploads"));
 
 if (!Directory.Exists(resourcesPath))
 {
@@ -362,8 +440,27 @@ if (!Directory.Exists(resourcesPath))
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(resourcesPath),
-    RequestPath = "/resources",
-    OnPrepareResponse = ctx => { ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "*"); }
+    RequestPath = "/uploads",
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+        ctx.Context.Response.Headers.Append("Accept-Ranges", "bytes");
+
+        // 自动设置常见视频格式的 Content-Type
+        var fileExtension = Path.GetExtension(ctx.File.Name).ToLowerInvariant();
+        switch (fileExtension)
+        {
+            case ".mp4":
+                ctx.Context.Response.Headers.ContentType = "video/mp4";
+                break;
+            case ".webm":
+                ctx.Context.Response.Headers.ContentType = "video/webm";
+                break;
+            case ".ogg":
+                ctx.Context.Response.Headers.ContentType = "video/ogg";
+                break;
+        }
+    }
 });
 
 #endregion
@@ -387,6 +484,9 @@ else
     });
 
     app.UseStaticFiles(); // 静态文件中间件
+
+    // 匹配 /admin 下所有请求，如果命中物理文件就直接返回；否则 fallback 到 admin/index.html
+    // app.MapFallbackToFile("/admin/", "admin/index.html");
 
     app.MapFallbackToFile("index.html");
 }
